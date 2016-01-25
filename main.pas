7e60342b87b08cc786bbc1f0b5512c50696ce92f
Janderson - JJ <ffjanderson@gmail.com>
2016-01-24 22:11:41 -0200

    bug fix * Hide window after 5 seconds. * Display time, as MM:SS * BallonTip as message of end pomodoro

--- main.pas
+++ main.pas
@@ -6,24 +6,28 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  ComCtrls, Menus, StdCtrls;
+  ComCtrls, Menus, StdCtrls, IniPropStorage, MMSystem;
 
 type
 
   { TfrmMain }
 
   TfrmMain = class(TForm)
+    IniPropStorage1: TIniPropStorage;
     Label1: TLabel;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
+    menMostrar: TMenuItem;
     pmeStart: TMenuItem;
     PopupMenu1: TPopupMenu;
     ProgressBar1: TProgressBar;
     tmrPomo: TTimer;
     TrayIcon1: TTrayIcon;
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
+    procedure menMostrarClick(Sender: TObject);
     procedure MenuItem1Click(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
@@ -31,7 +35,7 @@ type
     procedure tmrPomoTimer(Sender: TObject);
   private
     var
-      time: integer;
+      intTimeSec: integer;
       frmClose: boolean;
   public
     { public declarations }
@@ -49,15 +53,22 @@ implementation
 procedure TfrmMain.tmrPomoTimer(Sender: TObject);
 begin
      ProgressBar1.Position:= ProgressBar1.Position - 1;
-     Label1.Caption:= IntToStr(ProgressBar1.Position);
-     if (ProgressBar1.Position=time-5) then
+     Label1.Caption:= IntToStr(ProgressBar1.Position div 60) + ':' + IntToStr(ProgressBar1.Position mod 60);
+     if (ProgressBar1.Position=intTimeSec-5) then
      begin
           frmMain.Close;
      end;
      if (ProgressBar1.Position<=0) then
      begin
        tmrPomo.Enabled:= False;
-       ShowMessage('terminou o pomo');
+       Label1.Caption:= 'Fim';
+       TrayIcon1.BalloonTitle:='terminou o pomo';
+       TrayIcon1.BalloonHint:='faça uma pausa';
+              sndPlaySound('C:\sounds\test.wav', snd_Async or snd_NoDefault);
+       frmMain.Show;
+       TrayIcon1.ShowBalloonHint;
+
+
      end;
 
 end;
@@ -65,8 +76,8 @@ end;
 procedure TfrmMain.pmeStartClick(Sender: TObject);
 begin
      frmMain.Show;
-     ProgressBar1.Max:= time;
-     ProgressBar1.Position:= time;
+     ProgressBar1.Max:= intTimeSec;
+     ProgressBar1.Position:= intTimeSec;
      tmrPomo.Enabled:= true;
 end;
 
@@ -82,7 +93,7 @@ var
 
 begin
   InputQuery('Time pomodoro', 'Time in Sec', s);
-  time := StrToInt(s);
+  intTimeSec := StrToInt(s);
 end;
 
 procedure TfrmMain.MenuItem3Click(Sender: TObject);
@@ -93,9 +104,14 @@ end;
 
 procedure TfrmMain.FormShow(Sender: TObject);
 begin
-  time := 3600;
-  tmrPomo.Enabled:= False;
   frmClose := False;
+  menMostrar.Visible:= False;
+
+end;
+
+procedure TfrmMain.menMostrarClick(Sender: TObject);
+begin
+  frmMain.Show;
 end;
 
 procedure TfrmMain.FormClose(Sender: TObject; var CloseAction: TCloseAction);
@@ -105,7 +121,15 @@ begin
     CloseAction:= caFree;
   end else begin
     CloseAction:= caHide;
+    menMostrar.Visible:= True;
   end;
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  intTimeSec := 1500;
+  tmrPomo.Enabled:= False;
+
 end;
 
 end.
